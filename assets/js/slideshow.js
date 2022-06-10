f0dccbb4d5e69c3df0787a27baa27b83493b3f89
sk <samirkabra1161@gmail.com>
2022-06-09 20:17:23 -0400

    Slideshow update, minor changes
    -Added Previous, Next button
    -Added more links from the channel
    -Added some keywords to the body of the home page

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -4,33 +4,61 @@ This Program creates a slideshow effect for the page by changing the image name 
 Author: Samir Kabra
 */ 
 
-const banner = document.getElementById("banner")
-const cta = document.getElementById("cta")
+const banner = document.getElementById("banner");
+const cta = document.getElementById("cta");
 
 // Enter time between slide switch, in seconds
-const timeInS = 5
+const timeInS = 5;
 
 // Path of image goes at the end of imageElements
-imageElements = 'url("images/light-bl.svg"), url("images/light-br.svg"), url("images/overlay.png"), url("slideshow/'
-let toChange = '9.jpg")'
+imageElements = 'url("images/light-bl.svg"), url("images/light-br.svg"), url("images/overlay.png"), url("slideshow/';
+let toChange = '9.jpg")';
 
 /**
  * The fileChanger function changes the name of the image file. 
  * @param toChange the name of the file to be changed 
  * @returns toChange the new filename
  */
-function fileChanger(toChange) {
-    fileAsArray = toChange.split("")
+function fileChanger(toChange, goBack) {
+    fileAsArray = toChange.split("");
     
     if (parseInt(fileAsArray[0]) < 9) {
-        oldNum = parseInt(fileAsArray[0])
-        newNum = oldNum + 1
-        fileAsArray.splice(0, 1 , newNum.toString())
+        if (goBack) {
+            oldNum = parseInt(fileAsArray[0]);
+
+            if(oldNum != 0){
+                newNum = oldNum - 1;
+            } else {
+                newNum = 9;
+            }
+
+            fileAsArray.splice(0, 1 , newNum.toString());
+        } else {
+            oldNum = parseInt(fileAsArray[0]);
+            newNum = oldNum + 1;
+            fileAsArray.splice(0, 1 , newNum.toString());
+        }
     } else {
-        fileAsArray.splice(0, 1, "0")
+        if(goBack) {
+            fileAsArray.splice(0, 1, "8");   
+        } else {
+        fileAsArray.splice(0, 1, "0");
     }
-    toChange = fileAsArray.join("")
-    return toChange
+    }
+    toChange = fileAsArray.join("");
+    return toChange;
+}
+
+function nextImage() {
+    var goBack = false
+    toChange = fileChanger(toChange, goBack);
+    backgroundChanger(toChange);
+}
+
+function previousImage() {
+    var goBack = true;
+    toChange = fileChanger(toChange, goBack);
+    backgroundChanger(toChange);
 }
 
 /**
@@ -38,17 +66,18 @@ function fileChanger(toChange) {
  * @param toChange the name of the file to be changed into
  */
 function backgroundChanger(toChange) {
-    banner.style.backgroundImage = imageElements + toChange
-    cta.style.backgroundImage  = imageElements + toChange
+    banner.style.backgroundImage = imageElements + toChange;
+    cta.style.backgroundImage  = imageElements + toChange;
 }
 
-timeInMS = timeInS * 1000
+timeInMS = timeInS * 1000;
 /**
  * setInterval changes files and backgrounds in a set interval. 
  */
 setInterval(function() {
-    toChange = fileChanger(toChange);
-    backgroundChanger(toChange)    
+    var goBack = false;
+    toChange = fileChanger(toChange, goBack);
+    backgroundChanger(toChange);    
 }, timeInMS)
 
 
